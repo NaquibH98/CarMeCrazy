bd8bc9538afae2fc2a6582c1f7fc32d73df08cbf
NaquibH98 <naquibhaziq98@gmail.com>
2024-06-24 13:45:21 +0800

    Added Remote Class and enabled internet connectivity

---
+++ app/src/main/java/com/example/carmecrazy/remote/ApiUtils.java
@@ -0,0 +1,19 @@
+package com.example.carmecrazy.remote;
+
+
+public class ApiUtils {
+
+    // REST API server URL
+    public static final String BASE_URL = "http://178.128.220.20/2022905103/api/";
+
+    // return UserService instance
+    public static UserService getUserService() {
+        return RetrofitClient.getClient(BASE_URL).create(UserService.class);
+    }
+
+    // return BookService instance
+    public static BookService getBookService() {
+        return RetrofitClient.getClient(BASE_URL).create(BookService.class);
+    }
+
+}
