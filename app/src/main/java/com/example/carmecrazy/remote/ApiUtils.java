cee376cbbf77b389d07309b1db0deb2c739432c5
NaquibH98 <naquibhaziq98@gmail.com>
2024-06-24 14:50:34 +0800

    Added Retrofit implementation

--- app/src/main/java/com/example/carmecrazy/remote/ApiUtils.java
+++ app/src/main/java/com/example/carmecrazy/remote/ApiUtils.java
@@ -4,7 +4,7 @@ package com.example.carmecrazy.remote;
 public class ApiUtils {
 
     // REST API server URL
-    public static final String BASE_URL = "http://178.128.220.20/2022905103/api/";
+    public static final String BASE_URL = "http://178.128.220.20/2022911085/api/";
 
     // return UserService instance
     public static UserService getUserService() {
