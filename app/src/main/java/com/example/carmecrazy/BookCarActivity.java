6d8321f8cb5c6b960d9c89e2d9bef05dd249ae9e
NaquibH98 <naquibhaziq98@gmail.com>
2024-07-11 00:02:52 +0800

    Added Booking Car Java Class

---
+++ app/src/main/java/com/example/carmecrazy/BookCarActivity.java
@@ -0,0 +1,27 @@
+package com.example.carmecrazy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.carmecrazy.model.Car;
+import com.example.carmecrazy.model.User;
+import com.example.carmecrazy.remote.ApiUtils;
+import com.example.carmecrazy.remote.CarService;
+import com.example.carmecrazy.sharedpref.SharedPrefManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BookCarActivity {
+
+}
